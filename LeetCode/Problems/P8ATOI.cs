f4cbf03fde3ec1bcce33a3273b620a17d41d0840
Andrej Nenadić <andrejnana445@gmail.com>
2024-04-25 22:00:21 +0200

    Completed P8 - ATOI and P9 - Palidrome Number

---
+++ LeetCode/Problems/P8ATOI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class P8ATOI : IProblemSolver<TestCaseWrapper<InputWrapper<string>, Wrapper<int>>, Wrapper<int>>
+    {
+        public TestCaseWrapper<InputWrapper<string>, Wrapper<int>>[] TestCases => [
+            //new("42", 42),
+            //new("   -42", -42),
+            //new("4193 with words", 4193),
+            //new("-2147483647", -2147483647),
+            new("-91283472332", -2147483648),
+        ];
+
+        public Wrapper<int> Solve(object testCaseInput)
+        {
+            if (testCaseInput is not InputWrapper<string> input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return ATOI(input);
+        }
+
+        public static int ATOI(string input)
+        {
+            if (input.Length == 0)
+                return 0;
+
+            int sign = 1;
+            int result = 0;
+            bool foundNumber = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int charCode = input[i];
+
+                if (charCode >= 48 && charCode <= 57)
+                {
+                    try
+                    {
+                        result = checked(result * 10 + (charCode - 48));
+                    }
+                    catch (OverflowException)
+                    {
+                        return sign < 0 ? int.MinValue : int.MaxValue;
+                    }
+                    foundNumber = true;
+                    continue;
+                }
+                else if (foundNumber)
+                    return sign * result;
+
+                if (charCode == 32)
+                    continue;
+                //Plus
+                else if (charCode == 43)
+                    foundNumber = true;
+                //Minus
+                else if (charCode == 45)
+                {
+                    sign = -1;
+                    foundNumber = true;
+                }
+                else
+                    return 0;
+            }
+
+            return sign * result;
+        }
+    }
+}
---
+++ LeetCode/Problems/P9PalidromeNumber.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCode.Problems
+{
+    public class P9PalidromeNumber : IProblemSolver<TestCaseWrapper<InputWrapper<int>, Wrapper<bool>>, Wrapper<bool>>
+    {
+        public TestCaseWrapper<InputWrapper<int>, Wrapper<bool>>[] TestCases { get; } = [
+                new(121, true),
+                new(-121, false),
+                new(10, false)
+            ];
+
+        public Wrapper<bool> Solve(object testCaseInput)
+        {
+            if (testCaseInput is not InputWrapper<int> input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return IsPalindromeNoString(input);
+        }
+
+        public static bool IsPalindrome(int input)
+        {
+            if (input < 0)
+                return false;
+
+            if (input < 10)
+                return true;
+
+            var inputString = input.ToString();
+            for (int i = 0; i < inputString.Length / 2; i++)
+            {
+                if (inputString[i] != inputString[^(i + 1)])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsPalindromeNoString(int input)
+        {
+            if (input < 0)
+                return false;
+
+            if (input < 10)
+                return true;
+
+            int t = input;
+            int reverse = 0;
+            while (t > 0)
+            {
+                reverse = reverse * 10 + t % 10;
+                t /= 10;
+            }
+
+            return reverse == input;
+        }
+    }
+}
