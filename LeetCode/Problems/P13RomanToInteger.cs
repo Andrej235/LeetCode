34ebf3b64c9d8ef82a5925937a03b0bd779aa2f8
Andrej Nenadić <andrejnana445@gmail.com>
2024-04-27 22:18:03 +0200

    Completed P13 - Roman to int conversion and P14 - Longest common prefix

---
+++ LeetCode/Problems/P13RomanToInteger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class P13RomanToInteger : IProblemSolver<TestCaseWrapper<InputWrapper<string>, Wrapper<int>>, Wrapper<int>>
+    {
+        public TestCaseWrapper<InputWrapper<string>, Wrapper<int>>[] TestCases => [
+                new("III", 3),
+                new("LVIII", 58),
+                new("MCMXCIV", 1994)
+            ];
+
+        public Wrapper<int> Solve(object testCaseInput)
+        {
+            if (testCaseInput is not InputWrapper<string> input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return RomanToInteger(input);
+        }
+
+        private readonly Dictionary<char, int> map = new()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        private int RomanToInteger(string input)
+        {
+            int result = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i + 1 < input.Length && map[input[i]] < map[input[i + 1]])
+                    result -= map[input[i]];
+                else
+                    result += map[input[i]];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Problems/P14LongestCommonPrefix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class P14LongestCommonPrefix : IProblemSolver<TestCaseWrapper<InputWrapper<string[]>, string>, string>
+    {
+        public TestCaseWrapper<InputWrapper<string[]>, string>[] TestCases => [
+                new(new(["flower","flow","flight"]), "fl"),
+                new(new(["dog","racecar","car"]), ""),
+                new(new(["ab", "a"]), "a"),
+            ];
+
+        public string Solve(object testCaseInput)
+        {
+            if (testCaseInput is not InputWrapper<string[]> input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return LongestCommonPrefix(input);
+        }
+
+        public string LongestCommonPrefix(string[] strs)
+        {
+            string first = strs[0];
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (i >= strs[j].Length || first[i] != strs[j][i])
+                        return first[..i];
+                }
+            }
+
+            return first;
+        }
+    }
+}
