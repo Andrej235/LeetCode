b04688064952ca4220058a1d0e2d09d2cbf4c7a0
Andrej Nenadić <andrejnana445@gmail.com>
2024-04-27 14:25:30 +0200

    Completed P11 - Water container and P12 - Integer to roman conversion

---
+++ LeetCode/Problems/P11WaterContainer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class P11WaterContainer : IProblemSolver<TestCaseWrapper<InputWrapper<int[]>, Wrapper<int>>, Wrapper<int>>
+    {
+        public TestCaseWrapper<InputWrapper<int[]>, Wrapper<int>>[] TestCases { get; } = [
+                new(new([1,8,6,2,5,4,8,3,7]), 49),
+                new(new([1,1]), 1),
+            ];
+
+        public Wrapper<int> Solve(object testCaseInput)
+        {
+            if (testCaseInput is not InputWrapper<int[]> input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return MaxArea(input);
+        }
+
+        public static int MaxArea(int[] heights)
+        {
+            int i = 0;
+            int j = heights.Length - 1;
+            int max = 0;
+
+            while (i < j)
+            {
+                var area = Math.Min(heights[i], heights[j]) * (j - i);
+                if (area > max)
+                    max = area;
+
+                if (heights[i] < heights[j])
+                    i++;
+                else
+                    j--;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCode/Problems/P12IntegerToRoman.cs
@@ -0,0 +1,66 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class P12IntegerToRoman : IProblemSolver<TestCaseWrapper<InputWrapper<int>, string>, string>
+    {
+        public TestCaseWrapper<InputWrapper<int>, string>[] TestCases { get; } = [
+                new(3, "III"),
+                new(58, "LVIII"),
+                new(1994, "MCMXCIV")
+            ];
+
+        public string Solve(object testCaseInput)
+        {
+            if (testCaseInput is not InputWrapper<int> input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return IntToRoman(input);
+        }
+
+        private readonly Dictionary<int, string> map = new()
+        {
+            { 1, "I" },
+            { 5, "V" },
+            { 10, "X" },
+            { 50, "L" },
+            { 100, "C" },
+            { 500, "D" },
+            { 1000, "M" }
+        };
+
+        public string IntToRoman(int input)
+        {
+            string roman = "";
+            int i = 0;
+            while (input > 0)
+            {
+                var digit = input % 10;
+                input /= 10;
+
+                var power = (int)Math.Pow(10, i++);
+
+                if (digit % 5 == 4)
+                {
+                    roman = map[power] + map[(digit + 1) * power] + roman;
+                }
+                else
+                {
+                    if (digit % 5 != 0 && digit % 5 < 4)
+                        for (int j = 0; j < digit % 5; j++)
+                            roman = map[power] + roman;
+
+                    if (digit / 5 == 1)
+                        roman = map[power * 5] + roman;
+                }
+            }
+
+            return roman;
+        }
+    }
+}
