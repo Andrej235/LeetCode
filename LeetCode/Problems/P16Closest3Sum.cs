d79ee3b2950b5316e5502e98c7834f4be6e650c9
Andrej Nenadić <andrejnana445@gmail.com>
2024-04-28 19:57:13 +0200

    Completed P16 - Closest 3 sum and P17 Letter combinations

---
+++ LeetCode/Problems/P16Closest3Sum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class P16Closest3Sum : IProblemSolver<TestCaseWrapper<P16Closest3Sum.Closest3SumInput, Wrapper<int>>, Wrapper<int>>
+    {
+        public TestCaseWrapper<Closest3SumInput, Wrapper<int>>[] TestCases => [
+                new(new([-1,2,1,-4], 1), 2),
+                new(new([0,0,0], 1), 0)
+            ];
+
+        public Wrapper<int> Solve(object testCaseInput)
+        {
+            if (testCaseInput is not Closest3SumInput input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return Closest3Sum(input.Nums, input.Target);
+        }
+
+        public int Closest3Sum(int[] nums, int target)
+        {
+            int closest = int.MaxValue;
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int j = i + 1;
+                int k = nums.Length - 1;
+                while (j < k)
+                {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (target == sum)
+                        return target;
+                    else
+                    {
+                        if (Math.Abs(sum - target) < Math.Abs(closest - target))
+                            closest = sum;
+
+                        if (sum > target)
+                            k--;
+                        else
+                            j++;
+                    }
+                }
+            }
+
+            return closest;
+        }
+
+        public record Closest3SumInput(int[] Nums, int Target) : ITestCaseInput;
+    }
+}
---
+++ LeetCode/Problems/P17LetterCombinations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class P17LetterCombinations : IProblemSolver<TestCaseWrapper<InputWrapper<string>, IList<string>>, IList<string>>
+    {
+        public TestCaseWrapper<InputWrapper<string>, IList<string>>[] TestCases => [
+                new("23", ["ad","ae","af","bd","be","bf","cd","ce","cf"]),
+                new("", []),
+                new("2", ["a","b","c"])
+            ];
+
+        public IList<string> Solve(object testCaseInput)
+        {
+            if (testCaseInput is not InputWrapper<string> input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return LetterCombinations(input);
+        }
+
+        private readonly Dictionary<char, string> map = new() {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+
+        public IList<string> LetterCombinations(string input)
+        {
+            if (input.Length < 1)
+                return [];
+
+            IList<string> combinations = [];
+            AddCombinations(combinations, "", input);
+            return combinations;
+        }
+
+        public void AddCombinations(IList<string> combinations, string currentCombination, string input)
+        {
+            bool add = input[1..].Length == 0;
+            foreach (char letter in map[input[0]])
+            {
+                if (add)
+                    combinations.Add(currentCombination + letter);
+                else
+                    AddCombinations(combinations, currentCombination + letter, input[1..]);
+            }
+        }
+    }
+}
