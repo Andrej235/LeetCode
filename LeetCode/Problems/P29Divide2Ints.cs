37f46b71c50312276a9603f576f6ad0c0814ed54
Andrej Nenadić <andrejnana445@gmail.com>
2024-05-04 22:22:43 +0200

    'Completed' P29 - Division without '/' and P30 Concatinated string substring

---
+++ LeetCode/Problems/P29Divide2Ints.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class P29Divide2Ints : IProblemSolver<TestCaseWrapper<P29Divide2Ints.Divide2IntsInput, Wrapper<int>>, Wrapper<int>>
+    {
+        public record Divide2IntsInput(int Dividend, int Divisor) : ITestCaseInput;
+
+        public TestCaseWrapper<Divide2IntsInput, Wrapper<int>>[] TestCases => [
+                new(new(10, 3), 3),
+                new(new(7, -3), -2),
+            ];
+
+        public Wrapper<int> Solve(object testCaseInput)
+        {
+            if (testCaseInput is not Divide2IntsInput input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return Divide(input.Dividend, input.Divisor);
+        }
+
+        public int Divide(int dividend, int divisor)
+        {
+            if (dividend == int.MinValue && divisor == -1)
+                return int.MaxValue;
+
+            long result = 0;
+            bool isPositive = (dividend < 0 == divisor < 0);
+
+            long longDividend = Math.Abs((long)dividend);
+            long longDivisor = Math.Abs((long)divisor);
+
+            while (longDividend >= longDivisor)
+            {
+                int i = 0;
+                long divisorTemp = longDivisor;
+
+                while (longDividend >= (divisorTemp << 1))
+                {
+                    divisorTemp <<= 1;
+                    i++;
+                }
+
+                result |= 1L << i;
+                longDividend -= divisorTemp;
+            }
+
+            if (!isPositive)
+                result = -result;
+
+            if (result > int.MaxValue || result < int.MinValue)
+                return int.MaxValue;
+
+            return (int)result;
+        }
+    }
+}
---
+++ LeetCode/Problems/P30Substring.cs
@@ -0,0 +1,91 @@
+﻿using LeetCode.Utility;
+
+namespace LeetCode.Problems
+{
+    //Basically just copied a solution from https://leetcode.com/problems/substring-with-concatenation-of-all-words/solutions/3680493/c-sliding-window-approach-with-explanation/
+    public class P30Substring : IProblemSolver<TestCaseWrapper<P30Substring.Input, IList<int>>, IList<int>>
+    {
+        public record Input(string S, string[] Words) : ITestCaseInput;
+
+        public TestCaseWrapper<Input, IList<int>>[] TestCases => [
+                new(new("barfoothefoobarman", ["foo","bar"]), [0, 9]),
+                new(new("wordgoodgoodgoodbestword", ["word","good","best","word"]), []),
+                new(new("barfoofoobarthefoobarman", ["bar","foo","the"]), [6,9,12]),
+            ];
+
+        public IList<int> Solve(object testCaseInput)
+        {
+            if (testCaseInput is not Input input)
+                return [];
+
+            return FindSubstring(input.S, input.Words);
+        }
+
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            IList<int> result = [];
+
+            int wordLength = words[0].Length;
+            int totalWords = words.Length;
+            int substringLength = wordLength * totalWords;
+
+            if (s.Length < substringLength)
+                return result;
+
+            Dictionary<string, int> wordCount = [];
+            foreach (string word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                    wordCount[word]++;
+                else
+                    wordCount[word] = 1;
+            }
+
+            for (int i = 0; i < wordLength; i++)
+            {
+                Dictionary<string, int> currentWordCount = [];
+                int wordsFound = 0;
+                int left = i;
+
+                for (int j = i; j <= s.Length - wordLength; j += wordLength)
+                {
+                    string currentWord = s.Substring(j, wordLength);
+
+                    if (wordCount.ContainsKey(currentWord))
+                    {
+                        if (currentWordCount.ContainsKey(currentWord))
+                            currentWordCount[currentWord]++;
+                        else
+                            currentWordCount[currentWord] = 1;
+
+                        wordsFound++;
+
+                        while (currentWordCount[currentWord] > wordCount[currentWord])
+                        {
+                            string leftWord = s.Substring(left, wordLength);
+                            currentWordCount[leftWord]--;
+                            wordsFound--;
+                            left += wordLength;
+                        }
+
+                        if (wordsFound == totalWords)
+                        {
+                            result.Add(left);
+                            string leftWord = s.Substring(left, wordLength);
+                            currentWordCount[leftWord]--;
+                            wordsFound--;
+                            left += wordLength;
+                        }
+                    }
+                    else
+                    {
+                        currentWordCount.Clear();
+                        wordsFound = 0;
+                        left = j + wordLength;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
