2d2ef23b15fe11c5535faa5b914cb3e5729dc7ce
Andrej Nenadić <andrejnana445@gmail.com>
2024-05-05 14:12:06 +0200

    Completed P237 - Delete a node in a linked list without it's head / root node, daily 5.5.24

---
+++ LeetCode/Problems/P237DeleteANodeInLinkedList.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.DataStructures;
+using LeetCode.Utility;
+
+namespace LeetCode.Problems
+{
+    public class P237DeleteANodeInLinkedList : IProblemSolver<TestCaseWrapper<InputWrapper<ListNode?>, ListNode?>, ListNode?>
+    {
+        public TestCaseWrapper<InputWrapper<ListNode?>, ListNode?>[] TestCases => [
+                new(ListNode.From([5,1,9]), ListNode.From([1,9])),
+                new(ListNode.From([1,9]), ListNode.From([9])),
+            ];
+
+        public ListNode? Solve(object testCaseInput)
+        {
+            if (testCaseInput is not InputWrapper<ListNode?> input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return DeleteNode(input!);
+        }
+
+        public ListNode DeleteNode(ListNode node)
+        {
+            node.Value = node.Next!.Value;
+            node.Next = node.Next!.Next;
+
+            return node;
+        }
+    }
+}
