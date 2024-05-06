7e55cc7120f00d50f28e6f6276e1eca6bfc9cf41
Andrej Nenadić <andrejnana445@gmail.com>
2024-05-06 12:27:09 +0200

    Completed P206 Reverse a linked list and P2487 Remove list nodes from list based on value, daily 5.6.2024

---
+++ LeetCode/Problems/P206ReverseLinkedList.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.DataStructures;
+using LeetCode.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class P206ReverseLinkedList : IProblemSolver<TestCaseWrapper<InputWrapper<ListNode?>, ListNode?>, ListNode?>
+    {
+        public TestCaseWrapper<InputWrapper<ListNode?>, ListNode?>[] TestCases => [
+                new(ListNode.From([1,2,3,4,5]), ListNode.From([5,4,3,2,1])),
+                new(ListNode.From(1, 2), ListNode.From(2, 1)),
+                new(ListNode.From(), ListNode.From()),
+            ];
+
+        public ListNode? Solve(object testCaseInput)
+        {
+            if (testCaseInput is not InputWrapper<ListNode?> input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return ReverseList(input);
+        }
+
+        private ListNode? ReverseList(ListNode? root)
+        {
+            if (root is null)
+                return null;
+
+            ListNode? result = null;
+            ListNode? current = root;
+            while (current != null)
+            {
+                var next = current.Next;
+                current.Next = result;
+                result = current;
+                current = next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Problems/P2487RemoveNodesFromListNode.cs
@@ -0,0 +1,65 @@
+﻿using LeetCode.DataStructures;
+using LeetCode.Utility;
+
+namespace LeetCode.Problems
+{
+    public class P2487RemoveNodesFromListNode : IProblemSolver<TestCaseWrapper<InputWrapper<ListNode?>, ListNode?>, ListNode?>
+    {
+        public TestCaseWrapper<InputWrapper<ListNode?>, ListNode?>[] TestCases => [
+                new(ListNode.From([5,2,13,3,8]), ListNode.From([13,8])),
+                new(ListNode.From([1, 1, 1, 1]), ListNode.From([1, 1, 1, 1])),
+            ];
+
+        public ListNode? Solve(object testCaseInput)
+        {
+            if (testCaseInput is not InputWrapper<ListNode?> input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return RemoveNodesFromList(input);
+        }
+
+        public ListNode? RemoveNodesFromList(ListNode? root)
+        {
+            if (root is null)
+                return null;
+
+            ListNode? prev = null;
+            ListNode? current = root;
+
+            while (current != null)
+            {
+                var next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+
+            current = prev!;
+
+            int max = current.Value;
+            while (current.Next != null)
+            {
+                if (current.Next.Value >= max)
+                {
+                    max = current.Next.Value;
+                    current = current.Next!;
+                }
+                else
+                    current.Next = current.Next?.Next;
+            }
+
+            current = prev;
+            prev = null;
+
+            while (current != null)
+            {
+                var next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+
+            return prev;
+        }
+    }
+}
