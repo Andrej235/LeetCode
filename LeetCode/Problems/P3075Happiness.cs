6151981d15833b7086600a3808c9b7a156fb86d0
Andrej Nenadić <andrejnana445@gmail.com>
2024-05-09 12:51:01 +0200

    Completed P3075 Happiness and children, daily 5.9.24

---
+++ LeetCode/Problems/P3075Happiness.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class P3075Happiness : IProblemSolver<TestCaseWrapper<P3075Happiness.P3075Input, Wrapper<long>>, Wrapper<long>>
+    {
+        public record P3075Input(int[] Happiness, int K) : ITestCaseInput;
+
+        public TestCaseWrapper<P3075Input, Wrapper<long>>[] TestCases => [
+                new(new([1,2,3], 2), 4),
+                new(new([1,1,1,1], 1), 1),
+                new(new([2,3,4,5], 1), 5),
+            ];
+
+        public Wrapper<long> Solve(object testCaseInput)
+        {
+            if (testCaseInput is not P3075Input input)
+                throw new ArgumentException(null, nameof(testCaseInput));
+
+            return MaximizeHappiness(input.Happiness, input.K);
+        }
+
+        public long MaximizeHappiness(int[] happiness, int k)
+        {
+            Array.Sort(happiness);
+            long sum = 0;
+            for (int i = 0; i < k; i++)
+                sum += Math.Max(happiness[^(i + 1)] - i, 0);
+
+            return sum;
+        }
+    }
+}
